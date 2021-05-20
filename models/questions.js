45403f44973a301e6923734d340d8ad759d48bee
moonimooni <munhw1408@gmail.com>
2021-05-20 13:56:48 +0900

    Modify : Choice 모델 삭제

--- models/questions.js
+++ models/questions.js
@@ -24,10 +24,20 @@ const Question = new Schema({
     requiredMin: Number,
     requiredMax: Number,
   },
-  choices: {
-    type: Array,
-    required: true,
-  },
+  choices: [
+    {
+      value: {
+        type: String,
+        required: true,
+      },
+      index: Number,
+      count: {
+        type: Number,
+        required: true,
+        default: 0,
+      },
+    },
+  ],
   labels: Schema.Types.Mixed,
 });
 
