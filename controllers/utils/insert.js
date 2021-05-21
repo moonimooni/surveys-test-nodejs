f8b8849a5229cda238805efbf209d9e4a3341963
moonimooni <munhw1408@gmail.com>
2021-05-21 18:17:29 +0900

    Modify : GET survey/:id 코드 단순화

--- controllers/utils/insert.js
+++ controllers/utils/insert.js
@@ -1,4 +1,4 @@
-const User = require("../models/users");
+const User = require("../../models/users");
 
 exports.insertCreatorInfo = (resolveValue, creatorKey) => {
   return User.findOne({ userKey: creatorKey })
